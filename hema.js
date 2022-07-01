d52a3364b945338bf67353fec27f595e9c20acb8
hemamane <hemamane5376@gmail.com>
2022-07-01 15:20:11 +0530

    pattern

---
+++ hema.js
@@ -0,0 +1,172 @@
+
+// let num = 5;
+
+// for(let i = 1; i<=5; i++){
+//   let stars = ``
+//   for(let j = 1; j<=5; j++){
+//   if(i==j||(i+j==6)){
+//      stars += "   *    "
+//   }
+//   else{
+//     stars += "       "
+//   }
+  
+  
+//   }console.log(stars)
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let num = 5;
+// let px = num;
+// let py = num;
+// for(let i = 1; i<5; i++){
+//   stars = ``
+//   for(let j = 1; j<5*2; j++){
+//     if(j==px||j==py){
+//        stars += "  * "
+//     }else stars +="    "
+   
+//   }px++
+//   py--
+//   console.log(stars)
+// }
+
+
+// px = 1;
+// py = num*2-1;
+// for(let i = 1; i<=5; i++){
+//   stars = ``
+//   for(let j = 1; j<=5*2; j++){
+//     if(j==px||j==py){
+//        stars += "  * "
+//     }else stars +="    "
+   
+//   }px++
+//   py--
+//   console.log(stars)
+// }
+
+
+
+
+
+
+
+
+
+// let num = 5;
+// let count = 1;
+// for(let i = 1; i<=5; i++){
+//   stars =``
+//   for(let j = 1; j<=2*i-j; j++){
+//     if(i==j||j==i-j){
+     
+//     } stars += count++
+//     stars +="  "
+   
+//   }console.log(stars)
+// }
+
+
+
+
+
+
+// let num = 5;
+// let count = 1;
+// for(let i = 1; i<=5; i++){
+//   stars =``
+//   for(let j = 1; j<=2*i-j; j++){
+//      stars += count++
+//     stars +="  "
+   
+//   }console.log(stars)
+// }
+
+
+
+
+
+
+// let num=5;
+// let count=0;
+// let a=0;b=1
+// for(let i=1;i<=num;i++){
+//   let stars="  "
+//   for(let j=1;j<=i;j++){
+//     stars=stars+" "+a;
+//     count= a+b;
+//     a=b;
+//     b=count
+//   }
+//   console.log(stars)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// n=3
+// px=n;
+// py=n;
+// for(let i=1;i<n;i++){
+//     let star = "";
+//     for(let j=1;j<=n*2;j++){
+//         if(j==px||j==py){
+//             star+="   *   "
+//         }else{
+//             star+="     "
+//         }
+//     }
+//     px++;
+//     py--;
+//     console.log(star)
+// }
+// px=1;
+// py=n*2-1;
+// for(let i=1;i<=n;i++){
+//     let star = "";
+//     for(let j=1;j<n*2;j++){
+//         if(j==px||j==py){
+//             star+="   *   "
+//         }else{
+//             star+="     "
+//         }
+//     }
+//     px++;
+//     py--;
+//     console.log(star)
+// }
+
+
+// 
+
+// let num = 50;
+
+// for(let i = 1; i<=50; i++){
+//   stars =``
+//   for(let j = 1; j<=i; j++){
+//      stars += "*"
+//     stars +="  "
+   
+//   }console.log(`happy coding   ${stars} `)
+// }
